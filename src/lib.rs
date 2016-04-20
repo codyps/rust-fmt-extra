d08ee84c33b3fd7b3fc03419a1da4b5659ed4a49
Cody P Schafer <dev@codyps.com>
2016-04-20 16:28:34 -0400

    hex string formatter & moderization

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use std::fmt;
-use std::ops::Deref;
+use std::ops::{Deref,DerefMut};
+use std::convert::From;
+use std::iter::IntoIterator;
 
+/// Given a str, display it as a posix-shell style single quoted string with no ambiguity.
 pub struct SingleQuotedStr<'a> {
     s: &'a str
 }
 
-impl<'b> SingleQuotedStr<'b> {
-    pub fn new<'a>(i: &'a str) -> SingleQuotedStr<'a> {
+impl<'b> From<&'b str> for SingleQuotedStr<'b> {
+    fn from(i: &str) -> SingleQuotedStr {
         SingleQuotedStr { s: i }
     }
 }
@@ -25,17 +28,22 @@ impl<'a> fmt::Display for SingleQuotedStr<'a> {
 
 impl<'a> Deref for SingleQuotedStr<'a> {
     type Target = str;
-    fn deref<'b>(&'b self) -> &'b Self::Target {
+    fn deref(&self) -> &Self::Target {
         self.s
     }
 }
 
-pub struct Seperated<'a, D: fmt::Display, X: fmt::Display, I: Iterator<Item=X>> {
+/// Display a given iterator over display-ables using by printing each display-able 1 display-able
+/// seperator.
+///
+/// NOTE: for now, the seperator is also emitted after the last display-able. This should be
+/// expected to change.
+pub struct Seperated<'a, D: fmt::Display, X: fmt::Display, I: IntoIterator<Item=X> + 'a> {
     sep: D,
     inner: &'a Fn() -> I,
 }
 
-impl<'a, D: fmt::Display, X: fmt::Display, I: Iterator<Item=X>> fmt::Display for Seperated<'a, D, X, I> {
+impl<'a, D: fmt::Display, X: fmt::Display, I: IntoIterator<Item=X>> fmt::Display for Seperated<'a, D, X, I> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for ref i in (self.inner)() {
             try!(write!(f, "{}{}", i, self.sep));
@@ -45,17 +53,55 @@ impl<'a, D: fmt::Display, X: fmt::Display, I: Iterator<Item=X>> fmt::Display for
     }
 }
 
-impl<'a, D: fmt::Display, X: fmt::Display, I: Iterator<Item=X>> Seperated<'a, D, X, I> {
+impl<'a, D: fmt::Display, X: fmt::Display, I: IntoIterator<Item=X> + 'a> Seperated<'a, D, X, I> {
     pub fn new(sep: D, inner: &'a Fn() -> I) -> Self {
         Seperated { sep: sep, inner: inner }
     }
 }
 
+/// A wrapper around anything that dereferences to a byte slice (`[u8]`) that displays it's
+/// contents as a hexidicimal string.
+#[derive(Clone, PartialEq, Eq)]
+pub struct Hs<T>(pub T);
+
+impl<T: Deref<Target=[u8]>> fmt::Display for Hs<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        for e in &self.0[..] {
+            try!(write!(fmt, "{:02x}", e));
+        }
+        Ok(())
+    }
+}
+
+impl<T: Deref<Target=[u8]>> fmt::Debug for Hs<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(fmt, "{}", self)
+    }
+}
+
+impl<T> Deref for Hs<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target
+    {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Hs<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[test]
+fn test_hs() {
+    assert_eq!(format!("{}", Hs(&b"hello"[..])), "68656c6c6f");
+}
 
 #[test]
 fn test_sqs() {
-    assert_eq!(format!("{}", SingleQuotedStr::new("'")), "''\\'''");
-    assert_eq!(format!("{}", SingleQuotedStr::new("a")), "'a'");
+    assert_eq!(format!("{}", SingleQuotedStr::from("'")), "''\\'''");
+    assert_eq!(format!("{}", SingleQuotedStr::from("a")), "'a'");
 }
 
 #[test]
